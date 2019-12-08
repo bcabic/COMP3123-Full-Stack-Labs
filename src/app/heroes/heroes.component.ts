4d8d2544f55b89a5f7acb100aa2d75f4ba4e3cd9
Billy Cabic <william.cabic@georgebrown.ca>
2019-12-08 03:35:26 -0500

    Completed Tour of Heroes tutorial 4-5

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
-  heroes = HEROES;
-
-  selectedHero: Hero;
-  
-  onSelect(hero:Hero): void{
-    this.selectedHero = hero;
-  }
-
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
